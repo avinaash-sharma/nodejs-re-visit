f77ee0206109415b964f51bd469bfc0d17aeeb88
Avinash Sharma <avi.rohit13@hotmail.com>
2024-10-17 05:45:01 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -29,6 +29,28 @@ app.delete('/user', (req, res) => {
   return res.send('DB call done, deleted successfully');
 })
 
+// this works for both /username & name
+app.get('/(user)?name', (req, res) => {
+  return res.send({ firstName: 'Avinash', lastName: 'Sharma' });
+})
+
+
+// complex usage of regex patterns - here it means that the pattern starts with anything but ends with hole is valid like asshole is valid
+app.get(/.*hole/, (req, res) => {
+  return res.send({ firstName: 'Avinash', lastName: 'Sharma' });
+})
+
+app.get('/userwithid', (req, res) => {
+  console.log("🚀 ~ app.get ~ req:", req.query);
+  return res.send({ firstName: 'Avinash', lastName: 'Sharma' });
+})
+
+app.get('/userwithparams/:userId', (req, res) => {
+  console.log("🚀 ~ app.get ~ req:", req.params);
+  console.log("🚀 ~ app.get ~ req:", req.query);
+  return res.send({ firstName: 'Avinash', lastName: 'Sharma', paramsPassed: req.params.userId, queryPassed: req.query.data });
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
