ce80b35fa0310c73aa179c8000cb88d7ca3cef51
Avinash Sharma <avi.rohit13@hotmail.com>
2024-10-19 05:24:28 +0530

    get users, get one by emailId etc

--- src/app.js
+++ src/app.js
@@ -7,20 +7,50 @@ const port = 7070
 
 app.use(express.json());
 
+// get all users
+
+app.get('/feed', async (req, res) => {
+
+  try {
+    const users = await User.find({});
+    if (users.length > 0) {
+      res.send(users);
+    } else {
+      res.status(404).send('No users found');
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
+
+})
+
 app.post('/signup', async (req, res) => {
   console.log("🚀 ~ app.post ~ req:", req.body);
 
-  const { firstName, lastName, emailId, password, age, gender } = req.body;
-
   const user = new User(req.body)
-
-
   try {
     // saving the user to db
     await user.save();
     res.send('User added successfully')
   } catch (error) {
     res.send(error)
+  }
+})
+
+app.get('/userbyemail', async (req, res) => {
+
+  console.log("🚀 ~ app.get ~ req.body.emailId:", req.body.emailId)
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId }, 'firstName');
+    res.send(user);
+
+    // if (user.length > 0) {
+    //   res.send(user);
+    // } else {
+    //   res.status(404).send('No user found');
+    // }
+  } catch (error) {
+    res.send(error.message);
   }
 })
 
